f586807c8693a82330c8fe5d78655e138893d628
Nguyễn Như Huy <nguyennhuhuy42@gmail.com>
2026-03-12 22:13:24 +0700

    Refactor: Add check Firebase initialization in firebase.config.ts

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,14 +1,20 @@
-import admin from 'firebase-admin';
+import admin from "firebase-admin";
 
+let app;
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
-  })
-})
+if (!admin.apps.length) {
+  app = admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
 
-console.log("PROJECT ID:", process.env.FIREBASE_PROJECT_ID)
+  console.log("Firebase initialized");
+  console.log("PROJECT ID:", process.env.FIREBASE_PROJECT_ID);
+} else {
+  app = admin.app();
+}
 
 export default admin;
